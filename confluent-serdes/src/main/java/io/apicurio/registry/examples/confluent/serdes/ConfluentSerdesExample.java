f9c92d39471abcde922ca3122342d5d634e3024d
brendonvicente <86419409+brendonvicente@users.noreply.github.com>
2023-01-25 17:44:27 +0000

    Update ConfluentSerdesExample.java
    Update to new version from CCOMPAT_API_URL

--- confluent-serdes/src/main/java/io/apicurio/registry/examples/confluent/serdes/ConfluentSerdesExample.java
+++ confluent-serdes/src/main/java/io/apicurio/registry/examples/confluent/serdes/ConfluentSerdesExample.java
@@ -68,7 +68,7 @@ import io.confluent.kafka.serializers.KafkaAvroSerializer;
 public class ConfluentSerdesExample {
 
     private static final String REGISTRY_URL = "http://localhost:8080/apis/registry/v2";
-    private static final String CCOMPAT_API_URL = "http://localhost:8080/apis/ccompat/v6";
+    private static final String CCOMPAT_API_URL = "http://localhost:8080/apis/ccompat/v7";
 
     private static final String SERVERS = "localhost:9092";
     private static final String TOPIC_NAME = ConfluentSerdesExample.class.getSimpleName();
